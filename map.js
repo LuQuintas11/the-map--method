0b195270640707d5601439cf9a75903055d6237d
LuQuintas11 <luciaquintas14@gmail.com>
2022-07-13 23:57:27 +0000

    aa

--- map.js
+++ map.js
@@ -3,18 +3,29 @@
  * To run this file in Gitpod, use the 
  * command node map.js in the terminal
  */
-
+//callback function
+//it is a function we pass a parameter to the method
 // Using a for loop
-
+let nums = [1,2,3,4,5];
+let results=[];
+for(let num of nums){
+  results.push(num*2);
+}
+console.group(results)
 
 // Using map()
-
+const multByTwo = function (num) {
+  return num * 2;
+}
+const mapResult =nums.map(multByTwo)
+console.log(mapResults)
 
 // Simplified w/ map()
 
+const Simplfied= nums.map(function(num){return num*2});
 
 // Simplfied w/ map() + arrow function
-
+const multByTwo=nums.map(num=>num*2) 
 
 // With objects:
 const students = [
@@ -37,3 +48,7 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+
+const studentWithIds=students.map(student=>(student.id, student.name))
+console.log(studentWithIds)
